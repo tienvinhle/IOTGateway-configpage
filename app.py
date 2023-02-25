2a046f08ece6754e711ead58f6100cc483be53b1
tienvinhle <tienvinh.industrial.automation@gmail.com>
2023-02-26 01:52:49 +0700

    expend to MQTT Cpnfg

--- app.py
+++ app.py
@@ -2,21 +2,38 @@ from flask import Flask, render_template, request, send_from_directory
 import os
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = '/etc/modbus'
+app.config['UPLOAD_MOSBUS_FOLDER'] = '/etc/modbus'
+app.config['UPLOAD_MQTT_FOLDER'] = '/etc/mqtt'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/uploadmodbus', methods=['POST'])
 def upload():
     file = request.files['file']
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    file.save(os.path.join(app.config['UPLOAD_MOSBUS_FOLDER'], file.filename))
     return 'File uploaded successfully'
 
-@app.route('/viewconfig')
+@app.route('/viewmodbusconfig')
 def view_config():
     filename = '/etc/modbus/deviceConfig.conf'
+    try:
+        with open(filename, 'r') as f:
+            config = f.read()
+    except FileNotFoundError:
+        config = 'No config file found'
+    return config
+
+@app.route('/uploadmqtt', methods=['POST'])
+def upload():
+    file = request.files['file']
+    file.save(os.path.join(app.config['UPLOAD_MQTT_FOLDER'], file.filename))
+    return 'File uploaded successfully'
+
+@app.route('/viewmqttconfig')
+def view_config():
+    filename = '/etc/mqtt/appConfig.conf'
     try:
         with open(filename, 'r') as f:
             config = f.read()
